1e63d5ed515b2489de971806f3a73c50eeda8efb
SereneYe <sereneye130@gmail.com>
2024-09-09 11:01:47 +1000

    11

--- js/fetchCrimeData.js
+++ js/fetchCrimeData.js
@@ -44,12 +44,12 @@ $(document).ready(function () {
       "https://a5c7zwf7e5.execute-api.ap-southeast-2.amazonaws.com/dev/lut";
     const offencesUrl =
       "https://a5c7zwf7e5.execute-api.ap-southeast-2.amazonaws.com/dev/offences";
-    const startDate = "08-25-2024";
-    const endDate = "08-26-2024";
+    const startDate = "01-25-2024";
+    const endDate = "07-26-2024";
 
     fetchGeographiesData(geographiesUrl).done(function (data) {
       // Filter out only the items where type is "POSTCODE"
-      const postcodeData = data.filter(item => item.type === "POSTCODE");
+      const postcodeData = data.filter((item) => item.type === "POSTCODE");
 
       const boundaryPromises = postcodeData.map((item) =>
         fetchOffencesData(offencesUrl, item.type, item.name, startDate, endDate)
