c209e42f737c9b42dff25f11f4b95fd013850a6f
PercyLina <tanpercy0701@gmail.com>
2024-09-11 09:08:23 +1000

    Update fakePropertyData.js

--- data/fakePropertyData.js
+++ data/fakePropertyData.js
@@ -8,48 +8,48 @@ const fakeDataList = [
       "Proudly positioned on a large 1,009sqm lot and nestled behind a private gated facade sits this gorgeous, family-friendly haven. ",
   },
   {
-    imgSrc: "media/blogs/blossom.jpg",
-    username: "-- 👩‍🔧 DreamyWanderer",
+    imgSrc: "https://i2.au.reastatic.net/1000x750/09194c17c67478a823b52a07a928d8ad4702b6e33dba6b83c1b7600f87ddac8a/image.jpg",
+    username: "-- 👩‍🔧 EstateWizard2333",
     comment:
       "Embrace the beauty of connections 🌸 and watch them blossom within our supportive community！✨",
     imageDescription:
       "A vibrant image symbolizing the blossoming connections within a community.",
   },
   {
-    imgSrc: "media/blogs/box-community-connect.jpg",
-    username: "-- 🧑🏼 WildHeartExplorer89",
+    imgSrc: "https://i2.au.reastatic.net/800x600/51db761d83960db50b4131a3d3311c86a19daf33561e3593c4ba6125747a8c3e/image.jpg",
+    username: "-- 🧑🏼 RealtyPro3434",
     comment:
       "Connect deeply with like-minded souls 😊 and foster a sense of belonging! 🌟",
     imageDescription:
       "An image representing the deep connections and sense of belonging within a community.",
   },
   {
-    imgSrc: "media/blogs/box-contact.png",
-    username: "-- 👩🏼‍🦱 SunnySoulSeeker32",
+    imgSrc: "https://i2.au.reastatic.net/800x600/ffa3a27fd4be6f7a9251871791dc1f274e813e28699db76d721c50e2ab451c34/image.jpg",
+    username: "-- 👩🏼‍🦱 HomeHero2333",
     comment:
       "Stay connected with your community 🏫 and let your light shine through every challenge. 😊",
     imageDescription:
       "An image illustrating the importance of staying connected within a community.",
   },
   {
-    imgSrc: "media/blogs/box-welcome-friday.jpg",
-    username: "-- 🧑🏼 HappyVibesOnly65",
+    imgSrc: "https://vuerealestate.com.au/wp-content/uploads/2024/06/633f08923c4c51e97e723cde_State-of-Luxury-Real-Estate-in-Partnership-with-AREAA-2048x1406-1-1024x703-1.jpeg 1024w, https://vuerealestate.com.au/wp-content/uploads/2024/06/633f08923c4c51e97e723cde_State-of-Luxury-Real-Estate-in-Partnership-with-AREAA-2048x1406-1-1024x703-1-300x206.jpeg 300w, https://vuerealestate.com.au/wp-content/uploads/2024/06/633f08923c4c51e97e723cde_State-of-Luxury-Real-Estate-in-Partnership-with-AREAA-2048x1406-1-1024x703-1-768x527.jpeg",
+    username: "-- 🧑🏼 PropertyPioneer",
     comment:
       "Join us for heartwarming gatherings every Friday 👯‍♂️👯 and strengthen the bonds of our community.",
     imageDescription:
       "An inviting image inviting people to join heartwarming community gatherings.",
   },
   {
-    imgSrc: "media/blogs/box-women-connect.jpg",
-    username: "-- 👩🏽‍🦰 EmpowermentQueen98",
+    imgSrc: "https://www.realestate.com.au/news-image/w_1024,h_768/v1659547674/news-lifestyle-content-assets/wp-content/production/capi_bf628398c82069429d3587c769e28c78_ec8812042acd007d81dce590bf6734cb.jpeg?_i=AA",
+    username: "-- 👩🏽‍🦰 HouseHunter",
     comment:
       "Empower and uplift each other through supportive networks 🤝 and create waves of change. 😃",
     imageDescription:
       "A powerful image representing women empowerment and supportive networks.",
   },
   {
-    imgSrc: "media/blogs/coffee-chatting.jpg",
-    username: "-- 🧔🏼‍♀️ CoffeeDreamer123",
+    imgSrc: "https://cdn.businessday.ng/2021/07/luxury-residential-real-estate.png",
+    username: "-- 🧔🏼‍♀️ DreamDweller",
     comment:
       "Sip on dreams and share inspirations over a cup of coffee ☕️, nurturing our community spirit. ⭐️",
     imageDescription:
@@ -57,7 +57,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/foot.jpg",
-    username: "-- 🧑🏻‍🦰 NatureLover456",
+    username: "-- 🧑🏻‍🦰 RealtyRanger0701",
     comment:
       "Take mindful steps towards well-being with refreshing walks in nature 🌳, igniting your soul. 🌱",
     imageDescription:
@@ -65,7 +65,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/forum.jpg",
-    username: "-- 👩🏽 ThoughtProvoker789",
+    username: "-- 👩🏽 LandLover0313",
     comment:
       "Engage in thought-provoking discussions and let ideas flow for a thriving community.",
     imageDescription:
@@ -73,7 +73,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/gym.jpg",
-    username: "-- 👱🏼‍♀️ FitnessWarrior321",
+    username: "-- 👱🏼‍♀️ HomeHarbor0331",
     comment:
       "Sweat it out together in group fitness activities 🏋🏻‍♀️ and build a stronger, healthier community.⛹🏼",
     imageDescription:
@@ -81,7 +81,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/gym2.jpg",
-    username: "-- 🧑🏻‍🦰 WellnessExplorer654",
+    username: "-- 🧑🏻‍🦰 PropertyPal987",
     comment:
       "Embark on a journey towards wellness together and create a hub of positive energy. 🤼‍♀️",
     imageDescription:
@@ -89,7 +89,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/laptab.jpg",
-    username: "-- 👩🏻‍🦳 TechSavvyAdventurer987",
+    username: "-- 👩🏻‍🦳 CasaCrusader456",
     comment:
       "Empower the community with digital skills and unlock new possibilities for growth.",
     imageDescription:
@@ -97,7 +97,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/nutrition.jpg",
-    username: "-- 👩🏼‍🦱 NutritionEnthusiast123",
+    username: "-- 👩🏼‍🦱 EstateExplorer123",
     comment:
       "Nourish the body and soul with insightful nutrition workshops 🍎, paving the way for well-being. 🥑",
     imageDescription:
@@ -105,7 +105,7 @@ const fakeDataList = [
   },
   {
     imgSrc: "media/blogs/workshop-2.jpg",
-    username: "-- 👩🏽‍🦰 MindfulnessSeeker456",
+    username: "-- 👩🏽‍🦰 PropertyPathfinder0621",
     comment:
       "Immerse yourself in mindfulness and unlock the secrets ✨ to a more balanced and fulfilling life. 😊",
     imageDescription:
