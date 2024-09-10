1ff390c47dcd7d8bbd80d0c5995c7a0f46dfb960
SereneYe <sereneye130@gmail.com>
2024-09-11 08:31:28 +1000

    commit

---
+++ data/fakePropertyData.js
@@ -0,0 +1,116 @@
+const fakeDataList = [
+  {
+    imgSrc: "media/blogs/workshop.jpg",
+    username: "-- 🧔🏼‍♀️ GoldenSunrise23",
+    comment:
+      "Let your soul bloom with wisdom 😎 in our empowering workshops🛠️. ",
+    imageDescription:
+      "An image of people participating in an empowering workshop.",
+  },
+  {
+    imgSrc: "media/blogs/blossom.jpg",
+    username: "-- 👩‍🔧 DreamyWanderer",
+    comment:
+      "Embrace the beauty of connections 🌸 and watch them blossom within our supportive community！✨",
+    imageDescription:
+      "A vibrant image symbolizing the blossoming connections within a community.",
+  },
+  {
+    imgSrc: "media/blogs/box-community-connect.jpg",
+    username: "-- 🧑🏼 WildHeartExplorer89",
+    comment:
+      "Connect deeply with like-minded souls 😊 and foster a sense of belonging! 🌟",
+    imageDescription:
+      "An image representing the deep connections and sense of belonging within a community.",
+  },
+  {
+    imgSrc: "media/blogs/box-contact.png",
+    username: "-- 👩🏼‍🦱 SunnySoulSeeker32",
+    comment:
+      "Stay connected with your community 🏫 and let your light shine through every challenge. 😊",
+    imageDescription:
+      "An image illustrating the importance of staying connected within a community.",
+  },
+  {
+    imgSrc: "media/blogs/box-welcome-friday.jpg",
+    username: "-- 🧑🏼 HappyVibesOnly65",
+    comment:
+      "Join us for heartwarming gatherings every Friday 👯‍♂️👯 and strengthen the bonds of our community.",
+    imageDescription:
+      "An inviting image inviting people to join heartwarming community gatherings.",
+  },
+  {
+    imgSrc: "media/blogs/box-women-connect.jpg",
+    username: "-- 👩🏽‍🦰 EmpowermentQueen98",
+    comment:
+      "Empower and uplift each other through supportive networks 🤝 and create waves of change. 😃",
+    imageDescription:
+      "A powerful image representing women empowerment and supportive networks.",
+  },
+  {
+    imgSrc: "media/blogs/coffee-chatting.jpg",
+    username: "-- 🧔🏼‍♀️ CoffeeDreamer123",
+    comment:
+      "Sip on dreams and share inspirations over a cup of coffee ☕️, nurturing our community spirit. ⭐️",
+    imageDescription:
+      "An image symbolizing the warmth and camaraderie of sharing coffee and conversations.",
+  },
+  {
+    imgSrc: "media/blogs/foot.jpg",
+    username: "-- 🧑🏻‍🦰 NatureLover456",
+    comment:
+      "Take mindful steps towards well-being with refreshing walks in nature 🌳, igniting your soul. 🌱",
+    imageDescription:
+      "A serene image depicting individuals taking mindful walks in nature.",
+  },
+  {
+    imgSrc: "media/blogs/forum.jpg",
+    username: "-- 👩🏽 ThoughtProvoker789",
+    comment:
+      "Engage in thought-provoking discussions and let ideas flow for a thriving community.",
+    imageDescription:
+      "An image representing engaging and thought-provoking community discussions.",
+  },
+  {
+    imgSrc: "media/blogs/gym.jpg",
+    username: "-- 👱🏼‍♀️ FitnessWarrior321",
+    comment:
+      "Sweat it out together in group fitness activities 🏋🏻‍♀️ and build a stronger, healthier community.⛹🏼",
+    imageDescription:
+      "An image illustrating group fitness activities and community bonding.",
+  },
+  {
+    imgSrc: "media/blogs/gym2.jpg",
+    username: "-- 🧑🏻‍🦰 WellnessExplorer654",
+    comment:
+      "Embark on a journey towards wellness together and create a hub of positive energy. 🤼‍♀️",
+    imageDescription:
+      "A motivating image encouraging individuals to embark on a journey towards wellness.",
+  },
+  {
+    imgSrc: "media/blogs/laptab.jpg",
+    username: "-- 👩🏻‍🦳 TechSavvyAdventurer987",
+    comment:
+      "Empower the community with digital skills and unlock new possibilities for growth.",
+    imageDescription:
+      "An image representing empowerment through digital skills.",
+  },
+  {
+    imgSrc: "media/blogs/nutrition.jpg",
+    username: "-- 👩🏼‍🦱 NutritionEnthusiast123",
+    comment:
+      "Nourish the body and soul with insightful nutrition workshops 🍎, paving the way for well-being. 🥑",
+    imageDescription:
+      "A colorful image symbolizing the importance of nutrition and well-being.",
+  },
+  {
+    imgSrc: "media/blogs/workshop-2.jpg",
+    username: "-- 👩🏽‍🦰 MindfulnessSeeker456",
+    comment:
+      "Immerse yourself in mindfulness and unlock the secrets ✨ to a more balanced and fulfilling life. 😊",
+    imageDescription:
+      "An image representing mindfulness practices for a balanced life.",
+  },
+];
+
+export default fakeDataList;
---
+++ js/property/property.js
@@ -0,0 +1,113 @@
+import fakeDataList from "../../data/fakePropertyData.js";
+
+// Initiate Masonry
+const masonry = document.querySelector(".container-photo");
+const divider1 = masonry.querySelector(".divider1");
+
+// variables
+let pageSize = 12;
+let cardCount = 0;
+let colHeights = [0, 0, 0, 0]; // each column's height
+let observer; // intersection observer
+
+function loadCard() {
+  for (let i = 0; i < pageSize; i++) {
+    createCard(i);
+  }
+}
+
+function observe(card) {
+  if (!observer) {
+    observer = new IntersectionObserver((entries) => {
+      if (entries.length === 1 && entries[0].isIntersecting) {
+        loadCard();
+        observer.unobserve(entries[0].target);
+      }
+    });
+  }
+  observer.observe(card);
+}
+
+function createCard(i) {
+  const fakeData = fakeDataList[i];
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("card");
+  cardElement.setAttribute("tabindex", "0");
+  // hide card at beginning
+  cardElement.style.position = "fixed";
+  cardElement.style.top = 0;
+  cardElement.style.left = 0;
+  cardElement.style.visibility = "hidden";
+
+  const cardMarkup = `
+      <img src="${fakeData.imgSrc}" class="card-img" alt=${fakeData.imageDescription} />
+      <div class="card-content">
+        
+          <p class="card-attribute">
+            <span>${fakeData.comment}</span>
+            
+          </p>
+          <a class="blog-link" href="events.html#371">👉See Event👈</a>
+          <p class="card-username">${fakeData.username}</p>
+      </div>
+  `;
+
+  cardElement.innerHTML = cardMarkup;
+  document.body.append(cardElement);
+
+  const cardIndex = cardCount;
+  const colIndex = (cardIndex + 1) % 3;
+  let cardHeight = cardElement.clientHeight;
+  if (cardHeight < 400) {
+    cardHeight = 400;
+  }
+  colHeights[colIndex] += cardHeight;
+  setMasonryHeight();
+  masonry.insertBefore(cardElement, divider1);
+  cardCount++;
+
+  cardElement.style.position = "";
+  cardElement.style.visibility = "";
+  cardElement.classList.add("show");
+
+  // Listen for the end of the animation
+  cardElement.addEventListener("animationend", () => {
+    // Remove the 'show' class and add the 'hover' class
+    cardElement.classList.remove("show");
+    cardElement.classList.add("card-hover");
+  });
+
+  if (i === pageSize - 1) {
+    observe(cardElement);
+  }
+}
+
+function updateCardHeight(imgElement) {
+  return new Promise((resolve) => {
+    const cardElement = imgElement.parentElement;
+    const contentElement = cardElement.querySelector(".card-content");
+    const imgHeight = imgElement.clientHeight;
+    const contentHeight = contentElement.clientHeight;
+    cardElement.style.height = `${imgHeight + contentHeight}px`;
+    resolve();
+  });
+}
+
+function setMasonryHeight() {
+  masonry.style.height = Math.max(...colHeights) + "px";
+}
+
+// handle window resize
+function resetMasonryHeight() {
+  colHeights = [0, 0, 0, 0];
+  document.querySelectorAll(".card").forEach((card, index) => {
+    const cardHeight = card.clientHeight;
+    const colIndex = (index + 1) % 3;
+    colHeights[colIndex] += cardHeight;
+  });
+  setMasonryHeight();
+}
+
+window.addEventListener("resize", resetMasonryHeight);
+//////////
+loadCard();
