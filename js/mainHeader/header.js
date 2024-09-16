cae70fbd169a35c3507ecda1ed4fb033b002b828
SereneYe <sereneye130@gmail.com>
2024-09-16 11:57:30 +1000

    commit

--- js/mainHeader/header.js
+++ js/mainHeader/header.js
@@ -6,6 +6,5 @@ const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("clicked");
   headerEl.classList.toggle("nav-open");
 });
