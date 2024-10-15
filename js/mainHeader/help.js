50955381a09f18b4444572afbd3845de552d6bc9
SereneYe <sereneye130@gmail.com>
2024-10-15 22:34:15 +1000

    fail to add the mobile nav add the reademe file

--- js/mainHeader/help.js
+++ js/mainHeader/help.js
@@ -29,3 +29,15 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector("#mobile-nav-button");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("clicked");
+  headerEl.classList.toggle("nav-open");
+});
